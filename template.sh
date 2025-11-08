5d86fea2a312ae3373b5d36b4a7aec26c848fd56
prathyu <saiprathyushh@gmail.com>
2025-11-09 01:54:46 +0530

    Folder structure Added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# creating directories
+mkdir -p src
+mkdir -p research
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch requirements.txt
+touch app.py
+touch setup.py
+touch research/trails.ipynb
+
+echo "Directory and Files created successfully!"
